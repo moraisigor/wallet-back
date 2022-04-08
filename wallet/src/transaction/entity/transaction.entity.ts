bc6ef1cdf60f36166fb5f894db866d208b4891c3
Igor Morais <igor.morais@hollowsoft.com>
2022-04-08 09:19:55 -0300

    minor adjustments

--- wallet/src/transaction/entity/transaction.entity.ts
+++ wallet/src/transaction/entity/transaction.entity.ts
@@ -1,14 +1,14 @@
 import { Entity, Column, CreateDateColumn, UpdateDateColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
 
 import { Kind } from './kind.entity'
-import { Account } from 'src/account/entity/account.entity'
+import { Account } from '../../account/entity/account.entity'
 
 @Entity()
 export class Transaction {
   constructor(transaction: Partial<Transaction>) {
     Object.assign(this, transaction)
   }
-  
+
   @PrimaryGeneratedColumn('uuid')
   id: string
 
