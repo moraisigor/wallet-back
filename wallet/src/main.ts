ee237df3505f385cdd8c532fd49ee43924e30e1c
Igor Morais <igor.morais@hollowsoft.com>
2022-04-18 23:15:32 -0300

    adding validation pipe

--- wallet/src/main.ts
+++ wallet/src/main.ts
@@ -1,13 +1,16 @@
 import 'reflect-metadata'
 
-import { AppModule } from './app.module'
-
 import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
 
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
 
+import { AppModule } from './app.module'
+
 const load = async () => {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
+
+  app.useGlobalPipes(new ValidationPipe())
 
   await app.listen(4000)
 }
