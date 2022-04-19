4cb05f1cec2d1480b1794cdc5611fff9d8a075ac
Igor Morais <igor.morais@hollowsoft.com>
2022-04-19 01:43:08 -0300

    minor refactor account module

--- wallet/src/account/account.module.ts
+++ wallet/src/account/account.module.ts
@@ -1,18 +1,20 @@
-import { Module } from '@nestjs/common'
+import { Module, forwardRef } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { TransferModule } from '../transfer/transfer.module'
 
 import { Account } from './entity/account.entity'
 
 import { AccountService } from './account.service'
+import { GetTransferCase } from 'src/account/case/get.transfer.case'
 import { CreateAccountCase } from './case/create.account.case'
 
 import { AccountController } from './account.controller'
 import { AccountRepository } from './repository/account.repository'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account])],
+  imports: [TypeOrmModule.forFeature([Account]), forwardRef(() => TransferModule)],
   exports: [AccountRepository],
-  providers: [AccountService, AccountRepository, CreateAccountCase],
+  providers: [AccountService, AccountRepository, GetTransferCase, CreateAccountCase],
   controllers: [AccountController],
 })
 export class AccountModule {}
