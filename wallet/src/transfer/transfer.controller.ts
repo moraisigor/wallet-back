f2de7797c2e451a91a7538e3bc245ad604db9f98
Igor Morais <igor.morais@hollowsoft.com>
2022-04-18 22:35:10 -0300

    some minor refactor

--- wallet/src/transfer/transfer.controller.ts
+++ wallet/src/transfer/transfer.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Get } from '@nestjs/common'
-
-import { Transfer } from './entity/transfer.entity'
+import { Controller, Post, Body } from '@nestjs/common'
 
 import { TransferService } from './transfer.service'
+
+import { CreateTransferRequest } from './request/create.transfer.request'
+import { CreateTransferResponse } from './response/create.transfer.response'
 
 @Controller('transfer')
 export class TransferController {
   constructor(private readonly service: TransferService) {}
+
+  @Post()
+  async create(@Body() request: CreateTransferRequest): Promise<CreateTransferResponse> {
+    const transfer = await this.service.create(request)
+
+    return new CreateTransferResponse(transfer)
+  }
 }
