f1c872d00f9033ca5e3b3611dacde86438bfc568
Igor Morais <igor.morais@hollowsoft.com>
2022-04-19 01:04:18 -0300

    adding new function on transfer repository interface

--- wallet/src/transfer/repository/transfer.repository.interface.ts
+++ wallet/src/transfer/repository/transfer.repository.interface.ts
@@ -2,5 +2,6 @@ import { Account } from '../../account/entity/account.entity'
 import { Transfer } from '../entity/transfer.entity'
 
 export interface ITransferRepository {
+  all(query: unknown): Promise<Transfer[]>
   create(sender: Account, receiver: Account, amount: number): Promise<Transfer>
 }
