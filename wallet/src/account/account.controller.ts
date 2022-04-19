273242b78306bcb50f6032415c5884f524e503bb
Igor Morais <igor.morais@hollowsoft.com>
2022-04-19 01:09:26 -0300

    implement get transfer by account api

--- wallet/src/account/account.controller.ts
+++ wallet/src/account/account.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Post, Body } from '@nestjs/common'
+import { Controller, Get, Post, Param, Body } from '@nestjs/common'
 
 import { AccountService } from './account.service'
 
+import { GetTransferResponse } from './response/get.transfer.response'
 import { CreateAccountRequest } from './request/create.account.request'
 import { CreateAccountResponse } from './response/create.account.response'
 
 @Controller('account')
 export class AccountController {
   constructor(private readonly service: AccountService) {}
+
+  @Get('/:id/transfer')
+  async get(@Param('id') id: string) {
+    const list = await this.service.transfer(id)
+
+    return list.map((transfer) => new GetTransferResponse(transfer))
+  }
 
   @Post()
   async create(@Body() request: CreateAccountRequest): Promise<CreateAccountResponse> {
