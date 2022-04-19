63bc1cb626f1a0900ecb4b164376cb31bcefa912
Igor Morais <igor.morais@hollowsoft.com>
2022-04-18 22:33:33 -0300

    minor adjustments

--- wallet/src/account/account.module.ts
+++ wallet/src/account/account.module.ts
@@ -2,12 +2,17 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { Account } from './entity/account.entity'
+
 import { AccountService } from './account.service'
 import { AccountController } from './account.controller'
+import { AccountRepository } from './repository/account.repository'
+
+import { CreateAccountCase } from './case/create.account.case'
 
 @Module({
   imports: [TypeOrmModule.forFeature([Account])],
-  providers: [AccountService],
+  exports: [AccountRepository],
+  providers: [AccountService, AccountRepository, CreateAccountCase],
   controllers: [AccountController],
 })
 export class AccountModule {}
