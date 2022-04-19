4f2fb1e57400f9b45a9e613e8d9621e64f4e414f
Igor Morais <igor.morais@hollowsoft.com>
2022-04-18 22:33:46 -0300

    minor adjustments

--- wallet/src/transfer/transfer.module.ts
+++ wallet/src/transfer/transfer.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { AccountModule } from 'src/account/account.module'
 
 import { Transfer } from './entity/transfer.entity'
+
 import { TransferService } from './transfer.service'
+import { CreateTransferCase } from './case/create.transfer.case'
+
 import { TransferController } from './transfer.controller'
+import { TransferRepository } from './repository/transfer.repository'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transfer])],
-  providers: [TransferService],
+  imports: [TypeOrmModule.forFeature([Transfer]), AccountModule],
+  providers: [TransferService, TransferRepository, CreateTransferCase],
   controllers: [TransferController],
 })
 export class TransferModule {}
